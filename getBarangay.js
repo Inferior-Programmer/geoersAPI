684a0bc7dd8fa0dc04c6ec804293c88591a5a031
Jeremy Cesista <43639805+Inferior-Programmer@users.noreply.github.com>
2024-04-04 23:25:50 +0800

    First My Commit

--- getBarangay.js
+++ getBarangay.js
@@ -1,7 +1,10 @@
 const GeoJsonGeometriesLookup = require('geojson-geometries-lookup');
-const geoJson = require('./Barangays.json');
+const geoJson = require('./lols2.json');
+const geoJson2 = require('./Barangays.json');
 
 function geos(lat, long) {
+  console.log(geoJson);
+  console.log(geoJson2);
   const glookup = new GeoJsonGeometriesLookup(geoJson)
 
   const point = {type: "Point", coordinates: [long, lat]}
